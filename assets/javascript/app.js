cf001241718fca109ee32c56f93620473fbce3b1
Anne Palangi <anne.palangi@gmail.com>
2019-09-05 18:35:39 -0400

    beginning after tutor

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+ 
+ 
+    var number = 5;
+    var intervalId;
+
+
+    function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    function grade() {
+
+
+        $( "input:checked" ).each(function() {
+            
+
+            $("#correct").html(
+                parseInt($("#correct").html(), 10) + 
+                parseInt($(this).val(), 10));
+
+            
+          });
+
+          $("#incorrect").html(
+            2 - parseInt($("#correct").html(), 10));
+    }
+
+    function decrement() {
+
+      number--;
+
+      $("#show-number").html("<h2>" + number + "</h2>");
+
+      if (number === 0) {
+
+        stop();
+        grade();
+        alert("Time Up!");
+      }
+    }
+
+    function stop() {
+
+      clearInterval(intervalId);
+    }
+
+    $( document ).ready(function() {
+        
+              run();
+});
+
+
+    
